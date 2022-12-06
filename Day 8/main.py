ad5a357675cad0b1c1acc46f47bd1f9b9fa95ab5
SarangKumar <sarangkumar1578@gmail.com>
2022-12-06 08:49:45 +0530

    Day 8 Completed

---
+++ Day 8/main.py
@@ -0,0 +1,7 @@
+a = 50 
+b = 3
+
+print("The value of", a, "+", b,"is", a+b)
+print("The value of", a, "-", b,"is", a-b)
+print("The value of", a, "/", b,"is", a/b)
+print("The value of", a, "*", b,"is", a*b)
